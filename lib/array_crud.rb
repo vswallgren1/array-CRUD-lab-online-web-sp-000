da6ac23049338a6684833a4c4ec259e5a7ac891a
Vince W <vincewallgren@me.com>
2020-12-01 04:09:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  create_an_empty_array = []
+  []
 end
 
 def create_an_array
-  create_an_array = ["I", "am", "really", "learning"]
+  ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
