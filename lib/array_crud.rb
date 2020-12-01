c5486a2b466039b8e3d92860b3c1be1ef10362e6
Vince W <vincewallgren@me.com>
2020-12-01 03:59:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Juventus", "Napoli", "Roma", "Lazio"]
+  array = ["Juventus", "Napoli", "Roma", "Lazio"]
 end
 
 def add_element_to_end_of_array(array, element)
